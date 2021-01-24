a2115ed98135f24f7c874e18d81b73e5e769103d
Johnystar <honzik@stefek.cz>
2021-01-24 04:25:26 +0100

    forgot to push this

---
+++ pluralkit/__init__.py
@@ -0,0 +1,13 @@
+"""
+PluralKit API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the PluralKit API.
+:copyright: (c) 2021-present Johnystar
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'pluralkit'
+__author__ = 'Johnystar'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present Johnystar'
+__version__ = '2021.01.23.1'
